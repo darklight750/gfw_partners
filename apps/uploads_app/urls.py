5a1432eb984f6c6b77c0ba6e50a6ccea5e6be07a
John Tang <jmtang1@uci.edu>
2017-05-26 02:11:51 -0700

    Add files via upload

--- apps/uploads_app/urls.py
+++ apps/uploads_app/urls.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from apps.uploads_app.views import list
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 app_name = 'uploads_app'
 urlpatterns = [
     url(r'^list$', list, name='list')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
