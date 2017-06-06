61d5f936ce43f87ab338ae14e1212fb99c879727
John Tang <jmtang1@uci.edu>
2017-06-05 19:31:06 -0700

    Add files via upload

--- apps/uploads_app/models.py
+++ apps/uploads_app/models.py
@@ -5,7 +5,7 @@ import datetime
 
 
 def get_upload_path(instance, filename):
-    return  '{0}/{1}'.format(instance.user.id, filename)
+    return  '{0}/{1}'.format(instance.user.email, filename)
 
 class Document(models.Model):
 
@@ -13,6 +13,8 @@ class Document(models.Model):
     user = models.ForeignKey(User)
     #docfile = models.FileField(upload_to= 'documents/'+ user. + '%Y/%m/%d')
     #docfile = models.FileField(upload_to= '/'.join(['documents', '%Y','%m','%d' ,str(instance.pk), filename]))
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    docfile = models.FileField(upload_to=get_upload_path)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+    def __unicode__(self):
+        return self.docfile.url
